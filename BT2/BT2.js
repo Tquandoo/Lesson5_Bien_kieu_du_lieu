7e49750fec7cffe2c9ed741b2c2edb539183fb20
Tquandoo <quantrungdo2401@gmail.com>
2023-05-26 19:51:58 +0700

    BT2

---
+++ BT2/BT2.js
@@ -0,0 +1,9 @@
+let R = prompt('Nhap ban kinh duong tron: ');
+let d = 2*R;
+let C = 2*Math.PI*R;
+let S = Math.PI*R*R;
+document.write('duong kinh: '+ d);
+document.write("<br>");
+document.write('chu vi: '+ C);
+document.write("<br>");
+document.write('dien tich: '+ S);
